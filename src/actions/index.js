cbd761f58304e6e3fb380d05fa8b1f51ae0ff267
therewillbecode <tomw08@gmail.com>
2017-04-24 07:07:00 +0100

    Added async actions for retrieving recipes from url

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,3 +1,27 @@
 export const REQUEST_RECIPES = 'REQUEST_RECIPES'
 export const RECEIVE_RECIPES = 'RECEIVE_RECIPES'
 export const REQUEST_RECIPES_ERR = 'REQUEST_RECIPES_ERR'
+
+
+export const requestRecipes = () => ({
+  type: REQUEST_RECIPES
+})
+
+export const receiveRecipes = json => ({
+  type: RECEIVE_RECIPES,
+  recipes: json
+})
+
+export const requestRecipesErr = (err) => ({
+  type: REQUEST_RECIPES_ERR,
+  err
+})
+
+
+const fetchRecipes = () => dispatch => {
+  dispatch(requestRecipes())
+  return fetch(`https://gist.githubusercontent.com/hart88/198f29ec5114a3ec3460/raw/8dd19a88f9b8d24c23d9960f3300d0c917a4f07c/cake.json`)
+    .then(response => response.json())
+    .then(json => dispatch(receiveRecipes(json)))
+    .catch(err => dispatch(requestRecipesErr(err)))
+}

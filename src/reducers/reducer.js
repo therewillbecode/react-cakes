c59b3e0975a9416fd5f12b3aa7d4d54f10701738
therewillbecode <tomw08@gmail.com>
2017-04-24 07:54:39 +0100

    Added initialState

--- src/reducers/reducer.js
+++ src/reducers/reducer.js
@@ -1,3 +1,37 @@
+import { combineReducers } from 'redux'
+import {
+  REQUEST_RECIPES, RECEIVE_RECIPES, REQUEST_RECIPES_ERR
+} from '../actions/index'
+
 const reducer = (state, action) => state
 
-export default reducer
+const initialState = {
+  isFetching: false,
+  items: []
+}
+
+const recipes = (state = initialState, action) => {
+  switch (action.type) {
+    case REQUEST_RECIPES:
+      return {
+        ...state,
+        isFetching: true,
+      }
+    case RECEIVE_RECIPES:
+      return {
+        ...state,
+        isFetching: false,
+        items: action.posts,
+      }
+    case REQUEST_RECIPES_ERR:
+      return {
+        ...state,
+        isFetching: false,
+        items: action.posts,
+      }
+    default:
+      return state
+  }
+}
+
+export default recipes
